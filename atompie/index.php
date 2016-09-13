c531bd173256fba1a4cb0b609c3a9bff74dcab30
risto <risto.kowaczewski@gmail.com>
2016-09-13 22:51:03 +0200

    routing

--- atompie/index.php
+++ atompie/index.php
@@ -1,5 +1,5 @@
 <?php
-use AtomPie\System\Context;
+use AtomPie\Core\FrameworkConfig;
 use AtomPie\System\Kernel;
 
 ini_set('error_reporting','on');
@@ -12,15 +12,13 @@ $oLoader = require $sVendorDir . 'autoload.php';
 
 require 'Routing.php';
 
-$oConfig = new \AtomPie\Core\FrameworkConfig(
+$oConfig = new FrameworkConfig(
     realpath(__DIR__.'/EndPoint'),
     "Hello"
 );
-$oDispatchManifest = \AtomPie\Core\Dispatch\DispatchManifest::factory($oConfig, $_REQUEST);
-$oContext = new Context($oDispatchManifest->getEndPoint(), $oConfig);
 
 // Kernel
 
 $oKernel = new Kernel($oLoader);
-$oResponse = $oKernel->boot($oConfig, $oDispatchManifest, $oContext);
+$oResponse = $oKernel->__boot($oConfig);
 $oResponse->send();
