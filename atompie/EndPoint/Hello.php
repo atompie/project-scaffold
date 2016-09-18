b7c68ecf39e797c3ce6994f552012b60a2807a83
risto <risto.kowaczewski@gmail.com>
2016-09-18 22:13:05 +0200

    accept

--- atompie/EndPoint/Hello.php
+++ atompie/EndPoint/Hello.php
@@ -1,13 +1,22 @@
 <?php
+use AtomPie\System\Dispatch\ClientConstrain;
 use AtomPie\System\EndPoint;
 use AtomPie\Web\Connection\Http\Header;
 use Example\Hello;
 use Example\Param\UserName;
 
-return function (EndPoint $endPoint) {
+return function (EndPoint $endPoint, ClientConstrain $client) {
+
+//    $client->hasContentType('application/json');
+    $client->accept('application/json');
+//    $client->hasMethod('get');
+
     $endPoint->setClassName(Hello::class);
     $endPoint->onContract(UserName::class)->fillBy(function ($Name) {
         return new UserName($Name);
     });
-    $endPoint->onResponse()->addHeader(Header::CONTENT_TYPE,'application/json');
+    /*
+    $endPoint->authorize(new Basic('risto','risto'));
+    */
+//    $endPoint->onResponse()->addHeader(Header::CONTENT_TYPE,'application/json');
 };
--- src/Example/Hello.php
+++ src/Example/Hello.php
@@ -1,12 +1,16 @@
 <?php
 namespace Example {
 
+    use AtomPie\AnnotationTag\Authorize;
+    use AtomPie\AnnotationTag\Client;
     use Example\Param\UserName;
 
     class Hello
     {
 
         /**
+         * @Client(Accept="application/json")
+         * @Authorize(AuthToken="risto:risto", AuthType="basic")
          * @param UserName $Name
          * @return string
          */
